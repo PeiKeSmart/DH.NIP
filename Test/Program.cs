d0db0b18e48a3e675e419224240e828d9e4d1168
猿人易 <qcjxberin@outlook.com>
2024-08-19 20:00:27 +0800

    添加项目配置、IP处理类及测试框架
    在 `.editorconfig` 文件中添加了多个配置项，包括缩进样式、文件编码、行尾符、C# 代码风格设置等。
    在 `.gitignore` 文件中添加了 Visual Studio 相关的忽略规则，忽略了调试、发布、构建目录和一些特定文件类型。
    在 `DH.NIP.sln` 文件中添加了 Visual Studio 解决方案文件的内容，包含了项目和解决方案配置。
    添加了 `global.json` 文件，指定了 .NET SDK 的版本和一些相关设置。
    在 `LICENSE` 文件中添加了 MIT 许可证。
    在 `NuGet.config` 文件中添加了 Azure 中国和 NuGet.org 的包源配置。
    在 `Readme.MD` 文件中添加了项目标题和简短描述。
    新增了 `publish-beta.yml` 文件，用于在推送到 `main` 分支时发布 Beta 版本的 GitHub Actions 工作流。
    新增了 `publish.yml` 文件，用于在推送带有 `v*` 标签时发布正式版本的 GitHub Actions 工作流。
    新增了 `test.yml` 文件，用于在推送或拉取请求时运行测试的 GitHub Actions 工作流。
    在 `DH.NIP.csproj` 文件中添加了项目配置，包括目标框架、程序集信息、包信息和依赖项。
    新增了 `IndexInfo.cs` 文件，定义了 `IndexInfo` 结构体。
    新增了 `Ip.cs` 文件，定义了 `Ip` 类，用于 IP 地址解析。
    新增了 `IpDatabase.cs` 文件，定义了 `IpDatabase` 类，用于处理 IP 数据库。
    新增了 `IpHelper.cs` 文件，定义了 `IpHelper` 静态类，提供 IP 地址转换的辅助方法。
    新增了 `IpResolver.cs` 文件，定义了 `IpResolver` 类，实现了 `IIPResolver` 接口，用于解析 IP 地址。
    在 `Program.cs` 中添加了多个 `using` 语句，并引入了 `NewLife` 库。新增了 `Program` 类及其 `Main` 方法，包含了内存清理和异常处理逻辑。还添加了两个测试方法 `Test1` 和 `Test2`，用于处理 IP 地址和数据库操作。
    在 `Test.csproj` 中创建了一个新的项目文件，设置了目标框架为 `net8.0`，并指定了输出路径。添加了对 `DH.NIP` 项目的引用。
    在 `FolderProfile.pubxml` 中创建了一个发布配置文件，指定了发布目录和发布协议。
    在 `BasicTest.cs` 中创建了一个新的测试类 `BasicTest`，但目前没有具体的测试方法。
    在 `IpHelperTests.cs` 中创建了一个新的测试类 `IpHelperTests`，包含了多个测试方法，用于测试 IP 地址转换和处理功能。
    在 `IpTests.cs` 中创建了一个新的测试类 `IpTests`，包含了多个测试方法，用于测试 IP 地址解析和多线程处理。
    在 `XUnitTest.csproj` 中创建了一个新的测试项目文件，设置了目标框架为 `net8.0`，并指定了输出路径。添加了多个 NuGet 包引用，包括 `xunit` 和 `Microsoft.NET.Test.Sdk`，以及对 `DH.NIP` 项目的引用。

---
+++ Test/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Runtime.InteropServices;
+using NewLife;
+using NewLife.IP;
+using NewLife.Log;
+using NewLife.Reflection;
+
+namespace Test;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Runtime.CreateConfigOnMissing = false;
+        XTrace.UseConsole();
+
+        try
+        {
+            Test2();
+        }
+        catch (Exception ex)
+        {
+            XTrace.WriteException(ex);
+        }
+
+        GC.Collect(2, GCCollectionMode.Forced, true, true);
+        if (Runtime.Windows)
+        {
+            var p = Process.GetCurrentProcess();
+            EmptyWorkingSet(p.Handle);
+        }
+
+        Console.WriteLine("OK!");
+        Console.ReadKey();
+    }
+
+    [DllImport("psapi.dll", SetLastError = true)]
+    public static extern Boolean EmptyWorkingSet(IntPtr hProcess);
+
+    static void Test1()
+    {
+        IpResolver.Register();
+
+        var ip = "47.100.59.126";
+        var addr = ip.IPToAddress();
+        XTrace.WriteLine(addr);
+    }
+
+    static void Test2()
+    {
+        Console.WriteLine("Start");
+        Console.ReadKey();
+
+        var ip = new Ip();
+        ip.Init();
+
+        var db = ip.Db;
+
+        for (var idx = 0u; idx < db.Count; idx++)
+        {
+            var (set, area, addr) = db.GetIndex(idx);
+
+            if (idx % 10000 == 0)
+            //if (addr.Contains("纯真") || addr.Contains("CZ") || area.Contains("纯真") || area.Contains("CZ"))
+            {
+                XTrace.WriteLine("{0} {1} {2} {3}\t{4}", idx, set.Start.ToStringIP(), set.End.ToStringIP(), area, addr);
+            }
+        }
+        Console.WriteLine("End");
+        Console.ReadKey();
+        GC.Collect(2, GCCollectionMode.Forced, true, true);
+    }
+}
---
+++ XUnitTest/IpTests.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NewLife;
+using NewLife.IP;
+using Xunit;
+
+namespace XUnitTest;
+
+public class IpTests
+{
+    static IpTests()
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        IpResolver.Register();
+
+        var addr = "39.144.10.35".IPToAddress();
+
+        //#if DEBUG
+        if (!"data/ip.gz".AsFile().Exists) Thread.Sleep(9000);
+        //#endif
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var addr = "39.144.10.35".IPToAddress();
+        var ss = addr.Split(' ');
+        Assert.Equal("中国–广东", ss[0]);
+
+        addr = "116.234.91.199".IPToAddress();
+        ss = addr.Split(' ');
+        Assert.Equal("中国–上海–上海", ss[0]);
+
+        addr = "61.160.219.25".IPToAddress();
+        ss = addr.Split(' ');
+        Assert.Equal("中国–江苏–常州–武进区", ss[0]);
+
+        addr = "123.14.85.208".IPToAddress();
+        ss = addr.Split(' ');
+        Assert.Equal("中国–河南–郑州", ss[0]);
+
+        addr = "113.220.60.29".IPToAddress();
+        ss = addr.Split(' ');
+        Assert.Equal("中国–湖南–邵阳", ss[0]);
+
+        addr = "124.239.170.77".IPToAddress();
+        ss = addr.Split(' ');
+        Assert.Equal("中国–河北–衡水", ss[0]);
+
+        addr = "112.74.79.65".IPToAddress();
+        ss = addr.Split(' ');
+        Assert.Equal("中国–广东–深圳", ss[0]);
+
+        addr = "218.87.90.59".IPToAddress();
+        ss = addr.Split(' ');
+        Assert.Equal("中国–江西–九江–永修县", ss[0]);
+
+        addr = "39.144.8.87".IPToAddress();
+        ss = addr.Split(' ');
+        Assert.Equal("中国–广东–深圳", ss[0]);
+
+        addr = "111.55.141.170".IPToAddress();
+        ss = addr.Split(' ');
+        Assert.Equal("中国–山西", ss[0]);
+    }
+
+    [Fact]
+    public void Test自治区()
+    {
+        var addr = "116.136.7.43".IPToAddress();
+        var ss = addr.Split(' ');
+        Assert.Equal("中国–内蒙古–赤峰", ss[0]);
+        Assert.Equal("", ss[1]);
+    }
+
+    [Fact]
+    public void Test多线程()
+    {
+        Parallel.For(0, 100, i =>
+        {
+            var addr = "116.136.7.43".IPToAddress();
+            var ss = addr.Split(' ');
+            Assert.Equal("中国–内蒙古–赤峰", ss[0]);
+            Assert.Equal("", ss[1]);
+        });
+    }
+}
